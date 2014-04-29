7882205550f943f542458aba3c0af646e35d7bc2
Seb <emartine@bates.edu>
2014-04-29 19:28:00 -0400

    world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+World::Application.config.secret_key_base = '7f81fbf52504539bccf3274da35ec0848512cf226824c9b5f8fcf72efc321181ecb7887a883ff33247090da180ae995ab95011487101a4f4847cd1d19b70e6dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+World::Application.config.session_store :cookie_store, key: '_world_session'
