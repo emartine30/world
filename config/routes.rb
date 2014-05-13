eaa9a860e9442bab4a81a086c1a0ac6036dfd3c5
Seb <emartine@bates.edu>
2014-05-13 11:01:27 -0400

    pins

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ World::Application.routes.draw do
   resources :pins
 
   get '/about' => "page#about"
+  get '/pins' => "page#index"
   root "page#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
